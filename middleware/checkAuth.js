50cac28eebe040bcc3309e8321d1ba21c7508c29
Deimoth <deimoth@gmail.com>
2017-01-09 11:22:33 +0500

    fuck it!

--- middleware/checkAuth.js
+++
@@ -1,8 +0,0 @@
-var HttpError = require('../error').HttpError;
-
-module.exports =  function (req, res, next) {
-    if (!req.session.user) {
-        return next(new HttpError(401, "You are not authorized!"));
-    }
-    next();
-};
