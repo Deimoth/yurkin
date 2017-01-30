181728ec944ea19f1e15046869768bdc5e9bb127
purtov <purtov@proitr.ru>
2017-01-30 11:59:16 +0500

    logger lil fixes

--- logger.js
+++ logger.js
@@ -1,11 +1,15 @@
 var log4js = require('log4js');
-
-log4js.loadAppender('file');
-log4js.addAppender(log4js.appenders.file('logs/all.log'), 'default');
+var fs = require('fs');
+var path = 'all.log';
 
 var defaultLoggingLevel = 'ALL';
 
 exports.getLogger = function(loggingLevel) {
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, '');
+  }
+  log4js.loadAppender('file');
+  log4js.addAppender(log4js.appenders.file(path), 'default');
   loggingLevel = loggingLevel || defaultLoggingLevel;
   var logger = log4js.getLogger('default');
   logger.setLevel(loggingLevel);
